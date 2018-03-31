792b41b647b65ea7468c8b7d4df54f51fb58493a
PlasticVader <plasticwood1212@gmail.com>
2018-04-01 02:10:53 +0300

    Single-use interactive installer

--- scripts/installer.pl
+++ scripts/installer.pl
@@ -1,18 +1,28 @@
 #!/usr/bin/env perl
-# This looks like a mess, but it was not intended for readability in the first place
+# This is a bit of a mess, but it was not intended for readability in the first place
+# Just list the options, read the input and let the thread worker download the script(s)
 use strict;
 use warnings;
 use threads;
+use sigtrap 'handler' => \&normsig_handler, qw{ untrapped normal-signals };
 use HTTP::Tiny;
 use FindBin qw{ $Bin $Script };
 my $buff;
+my $term        = 0;
 my $usr1        = 0;
 my $selfunlink  = ($ENV{USER} =~ /^plast/) ? 0 : 1;
 my $base        = q{https://raw.githubusercontent.com/PlasticVader/my_cli_tools/master/scripts};
 my $bin         = qq{$ENV{HOME}/bin};
 my $bashrc      = qq{$ENV{HOME}/.bashrc};
 my $export_path = q{export PATH="$HOME/bin:$PATH"};
-@SIG{qw{INT TERM HUP}} = (q{IGNORE}, q{IGNORE}, q{IGNORE});
+
+sub normsig_handler {
+    exit if not threads->list;
+    $term++;
+    print qq{Termination attempt no. $term\n};
+    return 1;
+}
+
 $SIG{USR1} = sub {
     $_->join for threads->list;
     $usr1 = 1;
@@ -20,18 +30,32 @@ $SIG{USR1} = sub {
         print qq{Self-destructing\n};
         unlink qq{$Bin/$Script};
     }
-    exit;
+    exit 0;
 };
-sub download_script {
+
+sub thrworker_download {
     my ($url, $path) = @_;
     HTTP::Tiny->new->mirror($url, $path);
     chmod oct q{0755}, $path if $path;
-    print qq{Downloaded: [$path]\n};
+    print qq{[Thread] Download finished: [$path]\r\n};
     return 1;
 }
-sub killusr1 {
-    kill q{USR1}, $$;
+
+sub thrworker_create {
+    my ($sub, $params) = @_;
+    threads->create(
+        \&{$sub},
+        $params->[0],
+        $params->[1],
+    );
+    return 1;
 }
+
+sub clear {
+    system q{/usr/bin/clear};
+    return 1;
+}
+
 if (not -d $bin) {
     mkdir $bin, oct q{0755};
     print qq{Directory: [$bin] did not exist and was created\n};
@@ -46,19 +70,19 @@ if (not $bin_in_path and -f $bashrc) {
     system qq{source $bashrc};
     print qq{Sourced: [$bashrc]};
 }
-else {
-    print qq{Directory: [$bin] is present in PATH\n};
-}
+
 print qq{Getting the available scripts from [$base/available]\n};
 my $res = HTTP::Tiny->new->get(qq{$base/available});
 my @available = split / /, $res->{content};
 if (@available) {
-    print qq{Fetch successful, launching the options menu\n};
+    print qq{Fetch successful, launching the options menu\n\n};
     undef $res;
 }
 else {
     die qq{Fetch failed!\n};
 }
+
+clear;
 INST_BEGIN:
 do {
     $buff  = qq{Please input on of the commands in parens (cmd)\n};
@@ -73,7 +97,7 @@ do {
         }
     }
     $buff .= q{  (.all)  - [} . (join q{ }, @available) . qq{"\n};
-    $buff .= qq{  (.r)    - reload program  \n};
+    $buff .= qq{  (.r)    - reload interface  \n};
     $buff .= qq{  (.q)    - exit program \n\n};
     print $buff and undef $buff;
     INST_INPUT:
@@ -81,37 +105,37 @@ do {
         chomp $i;
         next INST_INPUT if $i =~ /^\s*$/;
         if ($i eq q{.q}){
-            killusr1
+            kill q{USR1}, $$
         }
         if ($i eq q{.all}) {
             for my $scr (@available) {
-                threads->create(
-                    \&download_script,
-                    qq{$base/$scr},
-                    qq{$bin/$scr},
+                thrworker_create(
+                    q{thrworker_download},
+                    [qq{$base/$scr}, qq{$bin/$scr}],
                 );
             }
             next INST_INPUT;
         }
         if ($i eq q{.r}) {
+            clear;
             goto INST_BEGIN;
         }
         if ($i !~ /^\d+$/) {
-            print qq{Invalid input => [$i]\n}
+            print qq{Invalid input => [$i]\r\n}
                 and next INST_INPUT;
         }
         if ($available[$i]) {
-            print qq{Downloading: [$available[$i]]\n};
-            threads->create(
-                \&download_script,
-                qq{$base/$available[$i]},
-                qq{$bin/$available[$i]},
+            thrworker_create(
+                q{thrworker_download},
+                [qq{$base/$available[$i]}, qq{$bin/$available[$i]}],
             );
             next INST_INPUT;
         }
         else {
-            print qq{Option id => [$i] is unavailable\n}
+            print qq{Option id => [$i] is unavailable\r\n}
                 and next INST_INPUT;
         }
     }
 } until ($usr1);
+
+
